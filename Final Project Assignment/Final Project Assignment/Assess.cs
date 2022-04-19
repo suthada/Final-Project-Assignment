5d674ec414d61eb988eeb332ff2a0ef52870ff48
suthada <96216472+suthada@users.noreply.github.com>
2022-04-19 17:02:02 +0700

    Project

---
+++ Final Project Assignment/Final Project Assignment/Assess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_Assignment
+{
+    internal class Assess //คิดเป็นราคาประเมินสิ่งปลูกสร้าง
+    {
+    }
+}
---
+++ Final Project Assignment/Final Project Assignment/Size.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_Assignment
+{
+    internal class Size // ขนาดพื้นที่สิ่งปลูกสร้าง 
+    {
+    }
+}
